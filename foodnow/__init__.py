59513e7bc2ea2e1b167c72c4d4efa62e86163f0d
2251052021duc <lehongduc201204@gmail.com>
2025-07-22 02:58:18 +0700

    change db

--- foodnow/__init__.py
+++ foodnow/__init__.py
@@ -7,7 +7,7 @@ import cloudinary
 
 app = Flask(__name__)
 app.secret_key = "KJGHJG^&*%&*^T&*(IGFG%ERFTGHCFHGF^&**&TYIU"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/fooddb?charset=utf8mb4" % quote('Admin@123')
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/fooddb?charset=utf8mb4" % quote('Admin@1234')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
