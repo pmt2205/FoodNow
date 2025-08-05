8c3e24bced41989099e812023ad524e942a3ee82
2251052021duc <lehongduc201204@gmail.com>
2025-08-06 00:11:15 +0700

    fix bug deploy to cloud

--- foodnow/models.py
+++ foodnow/models.py
@@ -121,7 +121,6 @@ class Order(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     address = db.Column(db.String(255))
     phone = db.Column(db.String(20))
-
     details = db.relationship('OrderDetail', backref='order', lazy=True)
 
     def calculate_total(self):
